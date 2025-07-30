30d4c6fdc12434f97e58700f814d312159ca10cf
varunpillala <varunoff543@gmail.com>
2025-07-30 23:24:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const quotes = [
-  "The best way to get started is to quit talking and begin doing.",
+  "The Best way to get started is to quit talking and begin doing.",
   "Don’t let yesterday take up too much of today.",
   "It’s not whether you get knocked down, it’s whether you get up.",
   "If you are working on something exciting, it will keep you motivated.",
